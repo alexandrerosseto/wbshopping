3687120c0f6dd5eb7380b0825506d74ffeef7e3f
alexandrerosseto <alexandrerosseto@gmail.com>
2020-05-11 16:17:18 -0300

    Initial version

---
+++ src/main/java/com/arosseto/wbshopping/WbshoppingApplication.java
@@ -0,0 +1,13 @@
+package com.arosseto.wbshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbshoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbshoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arosseto/wbshopping/WbshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arosseto.wbshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbshoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
