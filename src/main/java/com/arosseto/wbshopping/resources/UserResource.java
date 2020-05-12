384a451fa514cebcfadcf120e242c7c082ee59bf
alexandrerosseto <alexandrerosseto@gmail.com>
2020-05-12 17:30:49 -0300

    User service layer and component registration

--- src/main/java/com/arosseto/wbshopping/resources/UserResource.java
+++ src/main/java/com/arosseto/wbshopping/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.arosseto.wbshopping.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arosseto.wbshopping.entities.User;
+import com.arosseto.wbshopping.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User user = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+	public ResponseEntity<List<User>> findAll() {
+		List<User> listUser = service.findAll();
+		return ResponseEntity.ok().body(listUser);
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = service.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
 }
