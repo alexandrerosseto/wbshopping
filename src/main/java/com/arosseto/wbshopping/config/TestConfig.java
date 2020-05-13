dbdb3586103328621e3d341d22fae16073bfb683
alexandrerosseto <alexandrerosseto@gmail.com>
2020-05-13 19:16:22 -0300

    Payment and one-to-one association

--- src/main/java/com/arosseto/wbshopping/config/TestConfig.java
+++ src/main/java/com/arosseto/wbshopping/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.arosseto.wbshopping.entities.Category;
 import com.arosseto.wbshopping.entities.Order;
 import com.arosseto.wbshopping.entities.OrderItem;
+import com.arosseto.wbshopping.entities.Payment;
 import com.arosseto.wbshopping.entities.Product;
 import com.arosseto.wbshopping.entities.User;
 import com.arosseto.wbshopping.entities.enums.OrderStatus;
@@ -78,5 +79,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-10T16:13:57Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
